e7f9896a7cb54684cbafc331ce3f93ce228162f4
Bas Horsting <bashorsting@storyteq.com>
2024-10-30 15:17:32 +0100

    log some rect

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -47,7 +47,7 @@ onMounted(() => {
   color: white;
   text-decoration: none rgb(3, 93, 167);
   line-height: 0;
-  font-size: 200px;
+  font-size: 55px;
   pointer-events: auto;
   align-items: center;
   font-family: Herokid;
