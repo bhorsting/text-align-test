969bbfc5a9437f23461cedc9902f47c3b5e53702
Bas Horsting <bashorsting@storyteq.com>
2024-10-30 13:46:34 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <div class="root">
+  <div class="test-div">
+    TEST
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.test-div {
+  position:absolute;
+  top:100px;
+  left:100px;
+  color: white;
+  font-size: 100px;
+  font-family: Herokid;
+}
+</style>
