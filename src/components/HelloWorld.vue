3d9ee778b51e2a15644bca186d16206cbdddde2e
Bas Horsting <bashorsting@storyteq.com>
2024-10-30 13:52:54 +0100

    inner span

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,18 +7,45 @@ defineProps<{ msg: string }>()
 <template>
   <div class="root">
   <div class="test-div">
-    TEST
+    <span>
+      <span class="inner-text">TEST</span>
+    </span>
   </div>
   </div>
 </template>
 
 <style scoped>
 .test-div {
-  position:absolute;
-  top:100px;
-  left:100px;
-  color: white;
-  font-size: 100px;
+  position: absolute;
+  transform: translateX(208px) translateY(291px) rotate(0deg) scale(1) skewX(0deg) skewY(0deg);
+  width: 514px;
+  height: 62px;
+  display: flex;
+  opacity: 1;
+  visibility: visible;
+  white-space: pre-wrap;
+  -webkit-text-stroke: 0px rgba(0, 0, 0, 0);
+  font-weight: normal;
+  font-style: normal;
+  text-transform: uppercase;
+  letter-spacing: 0px;
+  overflow-wrap: break-word;
+  justify-content: center;
+  text-align: center;
+  color: rgb(3, 93, 167);
+  text-decoration: none rgb(3, 93, 167);
+  line-height: 0;
+  font-size: 35px;
+  pointer-events: auto;
+  align-items: center;
   font-family: Herokid;
+}
+
+.inner-text {
+  background-image: unset;
+  -webkit-text-fill-color: unset;
+  background-clip: unset;
+  overflow-wrap: break-word;
+  text-shadow: rgba(0, 0, 0, 0) 0px 0px 0px;
 }
 </style>
